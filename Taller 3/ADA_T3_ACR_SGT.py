0d9f977f5474e15b58a6a2a1b66c4e2e3d6841d1
alejocrrs <alejocrrs@gmail.com>
2022-10-13 21:32:22 -0500

    ACTUALIZACION FINAL

--- Taller 3/ADA_T3_ACR_SGT.py
+++ Taller 3/ADA_T3_ACR_SGT.py
@@ -6,26 +6,13 @@
 from urllib.request import urlopen
 import json
 from metodos_ordenamiento import *
-#Incluir archivo https://github.com/alejocrrs/ADA/blob/main/metodos_ordenamiento.py en la misma carpeta
 
-respuesta = urlopen("https://raw.githubusercontent.com/dwn84/ADA2022-2/main/little_data")
+#Incluir archivo metodos_ordenamiento.py en la misma carpeta
+#Enlace del archivo: https://github.com/alejocrrs/ADA/blob/main/Taller%203/metodos_ordenamiento.py
 
-datos_json = json.loads(respuesta.read())
-
-data = datos_json['data']
-data0 = datos_json['data0']
-data1 = datos_json['data1']
-data2 = datos_json['data2']
-data3 = datos_json['data3']
-data4 = datos_json['data4']
-data5 = datos_json['data5']
-data6 = datos_json['data6']
-data7 = datos_json['data7']
-data8 = datos_json['data8']
-data9 = datos_json['data9']
-data10 = datos_json['data10']
-data11 = datos_json['data11']
-
+def datos():
+    respuesta = urlopen("https://raw.githubusercontent.com/dwn84/ADA2022-2/main/little_data")
+    return json.loads(respuesta.read())
 
 def solo_enteros(lista):
     for e in lista:
@@ -56,8 +43,37 @@ def esta_ordenado(lista):
     else:
         return False
 
-def analizar_data():
-    pass
 
-print(tiempo_ejecucion(buble_sort, data11))
+def main():
+    funcs = {"Bubble": bubble_sort, 
+             "Insertion": insertion_sort, 
+             "Selection": selection_sort, 
+             "Quick": quick_sort,
+             "Merge": merge_sort, 
+             "Heap": heap_sort, 
+             "Counting": counting_sort,
+             "Radix": radix_sort}
     
+    for key in datos():
+        print(f"\nPARA LA LISTA '{key}' CON {len(datos()[key])} ELEMENTOS:")
+        
+        if solo_enteros(datos()[key]):
+            for keyf in funcs:
+                if (keyf == "Counting" or keyf == "Radix") and (not solo_positivos(datos()[key])):
+                    print(f"    # {keyf}: [!] ERROR: Lista con enteros negativos")
+                elif (keyf == "Quick" and (len(datos()[key]) > 2000)):
+                    lista = (datos()[key])[:2000]
+                    ordi = esta_ordenado(lista)
+                    tiempo = round(tiempo_ejecucion(funcs[keyf], lista), 5)
+                    ordf = esta_ordenado(lista)
+                    print(f"    # {keyf}: {tiempo} (Antes: {ordi} | Después: {ordf}) [PRIMEROS 2000 ELEMENTOS]")
+                else:
+                    lista = datos()[key]
+                    ordi = esta_ordenado(lista)
+                    tiempo = round(tiempo_ejecucion(funcs[keyf], lista), 5)
+                    ordf = esta_ordenado(lista)
+                    print(f"    # {keyf}: {tiempo} (Antes: {ordi} | Después: {ordf})")
+        else:
+            print("  [!] ERROR: La lista tiene valores que no son enteros")
+
+main()
