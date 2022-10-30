fcca56a2809deebec12204bafc6232271e25a4e4
alejocrrs <alejocrrs@gmail.com>
2022-10-29 21:09:44 -0500

    Update ADA_T4_ACR_Greedy_Florist.py

--- Taller 4/ADA_T4_ACR_Greedy_Florist.py
+++ Taller 4/ADA_T4_ACR_Greedy_Florist.py
@@ -1,13 +1,9 @@
 def getMinimumCost(k, c):
+    c.sort(reverse=True)
     costo = 0
-    n = len(c)
-    m = 1
-    
-    c.reverse()
-    
-    for i in range(n):
-        costo += c[i] * m
-        if (i + 1) % k == 0:
-            m += 1
-            
+    for i in range(len(c)):
+        costo += (int(i/k) + 1) * c[i]
     return costo
+
+
+print(getMinimumCost(3, [2,5,6]))
